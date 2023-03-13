31b1a013f259f2893d04ea393e7f0f0685fe98b5
nielshp14 <niels.s.risager@gmail.com>
2023-03-13 09:42:51 +0100

    Addet ttx

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+
+# this is our file
+import textToSpeach
+
 import numpy
 import math
+import keyboard
+import time
+
+
+alreadyStarted = False
+
+while (keyboard.is_pressed("esc") == False):
+    startButton = keyboard.is_pressed("å") and keyboard.is_pressed("ctrl")
+    
+    if(startButton and alreadyStarted == False):
+        textToSpeach.speakSelectet()
+        alreadyStarted = True
+    
+    if(startButton == False):
+        alreadyStarted = False
+    
+    # stops the program from using too much computing power
+    time.sleep(0.05)
+    
