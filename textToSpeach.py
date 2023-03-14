91e4bd1e0a7128e49cddadf88a435e6f9770447c
nielshp14 <niels.s.risager@gmail.com>
2023-03-14 15:51:14 +0100

    Added more voices voices and extra

--- textToSpeach.py
+++ textToSpeach.py
@@ -3,10 +3,13 @@ import pyperclip
 import pyttsx3
 import time
 import re
+import keyboard
+from concurrent.futures import ThreadPoolExecutor
 
 # creates ttx engine
 engine = pyttsx3.init()
 
+canseled = False
 
 alphabets= "([A-Za-z])"
 prefixes = "(Mr|St|Mrs|Ms|Dr)[.]"
@@ -17,7 +20,7 @@ websites = "[.](com|net|org|io|gov)"
 digits = "([0-9])"
 
 # this funktion was taken form https://stackoverflow.com/questions/4576077/how-can-i-split-a-text-into-sentences made by user D Greenberg 19 july 2015
-def split_into_sentences(text):
+def split_into_sentences(text) -> list:
     text = " " + text + "  "
     text = text.replace("\n"," ")
     text = re.sub(prefixes,"\\1<prd>",text)
@@ -45,21 +48,71 @@ def split_into_sentences(text):
     sentences = [s.strip() for s in sentences]
     return sentences
 
-def speakSelectet():
+def setWordsPerMinut(wordsPerMinut: int):
+    engine.setProperty('rate', wordsPerMinut)
+    
+def setLanguage(language:str) -> None:
+    voices = engine.getProperty('voices')
+    
+    danishId = ""
+    englishId = ""
+    for voice in voices:
+        print(voice.name)
+        if(voice.name == "Microsoft Helle - Danish (Denmark)"):
+            danishId = voice.id
+        elif (voice.name == "Microsoft Hazel Desktop - English (Great Britain)"):
+            englishId = voice.id
+            
+    print("Danish: " + danishId)
+    print("English: " + englishId)
+    
+    if (language == "danish"):
+        engine.setProperty('voice', danishId)
+        print("Danish set")
+    
+    if (language == "english"):
+        engine.setProperty('voice', englishId)
+        print("Englis set")
+
+# funktion is beeing called on a seperate thread 
+def chekForCanseled() -> None:
+    global canseled
+    while (True):
+        cansleShortCut = keyboard.is_pressed("p")
+        if(cansleShortCut or canseled):
+            print("stoping the speach")
+            canseled = True
+            break
+        time.sleep(0.01)
+    
+
+def speakSelectet() -> None:
+    global canseled 
+    canseled = False
+    
     # copy selectet text
     pyautogui.hotkey('ctrl','c')
     time.sleep(0.01)
     clibordText = pyperclip.paste()
     
-    # make sure the clibordtext ends with either '.','!' or '?'
+    # makes sure the clibordtext ends with either '.','!' or '?'
     lastChar = clibordText[len(clibordText)-1]
     if (not (lastChar == '.' or lastChar == '!' or lastChar == '?')):
         clibordText += '.'
     
     sentences = split_into_sentences(clibordText)
     
-    # Say sentences on at a time
-    for sentence in sentences:
-        print("saying: " + str(sentence))
-        engine.say(sentence)
-        engine.runAndWait()
+    with ThreadPoolExecutor() as executer:
+        # calls chekForCanseled on a seperate thread
+        executer.submit(chekForCanseled)
+        
+        # Say sentences on at a time
+        for sentence in sentences:
+            if (canseled):
+                break
+            print("saying: " + str(sentence) + " \n")
+            engine.say(sentence)
+            engine.runAndWait()
+        
+        # Stopes the second tread
+        canseled = True
